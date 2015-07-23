1792b97e1a376b377d715edc943c7ea57f79dab8
xiaoxiaojw21 <1207862646@qq.com>
2015-07-23 17:37:46 +0800

    1111111111

--- app/src/main/java/com/jash/chanelldemo/MainActivity.java
+++ app/src/main/java/com/jash/chanelldemo/MainActivity.java
@@ -9,8 +9,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 
 }
